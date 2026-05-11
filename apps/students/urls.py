a4596a526931fdc2f2fdc7064210723e5c75fa68
edzen12 <oichiev.edzen@gmail.com>
2026-05-11 19:05:04 +0600

    students urls

--- apps/students/urls.py
+++ apps/students/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from apps.students.views import (
+    GroupViewSet, StudentViewSet, ContactPersonViewSet
+)
+
+router = DefaultRouter()
+
+router.register('groups', GroupViewSet)
+router.register('students', StudentViewSet)
+router.register('contacts', ContactPersonViewSet)
 
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
